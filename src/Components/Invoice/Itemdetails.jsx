be2542b0bec8f1a1d42778527484b8c0cce2afa8
Rahul Raman <165632322+RahulNextBill@users.noreply.github.com>
2025-09-28 15:02:00 +0530

    sdsd

--- src/Components/Invoice/Itemdetails.jsx
+++ src/Components/Invoice/Itemdetails.jsx
@@ -1,69 +1,74 @@
-import React from "react"
+import React from "react";
 
 const Itemdetails = (props) => {
-	const { formData, setFormData, handleValueChange } = props
+  const { formData, setFormData, handleValueChange } = props;
 
-	const handleAddItem = () => {
-		setFormData((prev) => {
-			return {
-				...prev,
-				items: [...prev.items, { product: "", quantity: 1, price: 0 }],
-			}
-		})
-	}
+  const handleAddItem = () => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        items: [...prev.items, { product: "", quantity: 1, price: 1 }],
+      };
+    });
+  };
 
-	const handleRemoveItem = (index) => {
-		setFormData((prev) => {
-			return {
-				...prev,
-				items: prev.items.filter((el, id) => {
-					if (id != index) {
-						return el
-					}
-				}),
-			}
-		})
-	}
+  const handleRemoveItem = (index) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        items: prev.items.filter((el, id) => {
+          if (id != index) {
+            return el;
+          }
+        }),
+      };
+    });
+  };
 
-	return (
-		<div>
-			{" "}
-			<div className='invoice-items'>
-				<h3>Invoice Items</h3>
-				{formData?.items?.map((item, index) => (
-					<div key={index} className='invoice-item'>
-						<input
-							type='text'
-							name='product'
-							placeholder='Product Name'
-							value={item.product}
-							required
-						/>
-						<input
-							type='number'
-							name='quantity'
-							placeholder='Qty'
-							value={item.quantity}
-							required
-						/>
-						<input
-							type='number'
-							name='price'
-							placeholder='Price'
-							value={item.price}
-							required
-						/>
-						<button type='button' onClick={() => handleRemoveItem(index)}>
-							❌ Remove
-						</button>
-					</div>
-				))}
-				<button type='button' onClick={() => handleAddItem()}>
-					➕ Add Item
-				</button>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div>
+      {" "}
+      <div className="invoice-items">
+        <h3>Invoice Items</h3>
+        {formData?.items?.map((item, index) => (
+          <div key={index} className="invoice-item">
+            <input
+              type="text"
+              name={`product`}
+              placeholder="Product Name"
+              value={item?.product}
+              onChange={(e) => handleValueChange(e, index)}
+              required
+            />
+            <input
+              type="number"
+              name="quantity"
+              placeholder="Qty"
+              value={item.quantity}
+              onChange={(e) => handleValueChange(e, index)}
+              required
+            />
+            <input
+              type="number"
+              name="price"
+              placeholder="Price"
+              value={item.price}
+              onChange={(e) => handleValueChange(e, index)}
+              required
+            />
+            {formData.items?.length > 1 ? (
+              <button type="button" onClick={() => handleRemoveItem(index)}>
+                ❌ Remove
+              </button>
+            ) : null}
+          </div>
+        ))}
+        <button type="button" onClick={() => handleAddItem()}>
+          ➕ Add Item
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Itemdetails
+export default Itemdetails;
--- src/Pages/Invoice.jsx
+++ src/Pages/Invoice.jsx
@@ -1,78 +1,111 @@
-import React, { useState } from "react"
-import "./CSS/Invoice.css"
-import Partydetails from "../Components/Invoice/Partydetails"
-import Itemdetails from "../Components/Invoice/Itemdetails"
+import React, { useState } from "react";
+import "./CSS/Invoice.css";
+import Partydetails from "../Components/Invoice/Partydetails";
+import Itemdetails from "../Components/Invoice/Itemdetails";
+import BankDetails from "../Components/Invoice/BankDetails";
 // import { customers } from "../Components/Assets/invoice_assets";
 
 const InvoiceForm = () => {
-	const [formData, setFormData] = useState({
-		custfullName: "",
-		custemail: "",
-		custaddress: "",
-		custgstin: "",
-		custstcd: "",
-		custpincode: "",
-		sellerfullName: "",
-		selleremail: "",
-		selleraddress: "",
-		sellergstin: "",
-		sellerstcd: "",
-		sellerpincode: "",
-		items: [],
-	})
+  const [formData, setFormData] = useState({
+    custfullName: "",
+    custemail: "",
+    custaddress: "",
+    custgstin: "",
+    custstcd: "",
+    custpincode: "",
+    sellerfullName: "",
+    selleremail: "",
+    selleraddress: "",
+    sellergstin: "",
+    sellerstcd: "",
+    sellerpincode: "",
+    items: [{ product: "", quantity: 1, price: 1 }],
+    accountHolder: "",
+    accountNumber: "",
+    ifscCode: "",
+  });
 
-	const handleValueChange = (e) => {
-		const { name, value } = e.target
-		setFormData((prev) => ({ ...prev, [name]: value }))
-	}
+  const getTotal = () => {
+    return (
+      formData?.items?.reduce((acc, curr) => {
+        return acc + curr?.quantity * curr?.price;
+      }, 0) || 0
+    );
+  };
 
-	return (
-		<div
-			style={{
-				// maxWidth: "800px",
-				width: "90vw",
-				margin: "auto",
-				border: "1px solid #ccc",
-				padding: "20px",
-				borderRadius: "8px",
-			}}>
-			<h2>Invoice Form</h2>
-			<form>
-				{/* Customer Details */}
-				<Partydetails
-					heading='cust info.'
-					setFormData={setFormData}
-					formData={formData}
-					handleValueChange={handleValueChange}
-					subject={"customer"}
-				/>
+  const handleValueChange = (e, index) => {
+    const { name, value } = e.target;
+    if (name == "product" || name == "quantity" || name == "price") {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          items: prev.items?.map((el, idx) => {
+            if (idx == index) {
+              return { ...el, [name]: value };
+            } else {
+              return el;
+            }
+          }),
+        };
+      });
+    } else setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
-				{/* seller Details */}
-				<Partydetails
-					heading='seller info.'
-					setFormData={setFormData}
-					formData={formData}
-					handleValueChange={handleValueChange}
-					subject={"seller"}
-				/>
-				<Itemdetails
-					formData={formData}
-					setFormData={setFormData}
-					handleValueChange={handleValueChange}
-				/>
+  return (
+    <div
+      style={{
+        // maxWidth: "800px",
+        width: "90vw",
+        margin: "auto",
+        border: "1px solid #ccc",
+        padding: "20px",
+        borderRadius: "8px",
+      }}
+    >
+      <h2>Invoice Form</h2>
+      <form>
+        {/* Customer Details */}
+        <Partydetails
+          heading="cust info."
+          setFormData={setFormData}
+          formData={formData}
+          handleValueChange={handleValueChange}
+          subject={"customer"}
+        />
 
-				{/* Items */}
+        {/* seller Details */}
+        <Partydetails
+          heading="seller info."
+          setFormData={setFormData}
+          formData={formData}
+          handleValueChange={handleValueChange}
+          subject={"seller"}
+        />
 
-				{/* Total */}
-				<div className='invoice-total'>
-					{/* <h3>Total: ${getTotal().toFixed(2)}</h3> */}
-				</div>
-				<button type='submit' className='generate-btn'>
-					Generate Invoice
-				</button>
-			</form>
-		</div>
-	)
-}
+        {/* Items */}
+        <Itemdetails
+          formData={formData}
+          setFormData={setFormData}
+          handleValueChange={handleValueChange}
+        />
 
-export default InvoiceForm
+        <BankDetails
+          formData={formData}
+          setFormData={setFormData}
+          handleValueChange={handleValueChange}
+        />
+
+        {/* Total */}
+        <div className="invoice-total">
+          <h3>Total: ₹ {getTotal()}</h3>
+        </div>
+
+        <button type="submit" className="generate-btn">
+          Generate Invoice
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InvoiceForm;
